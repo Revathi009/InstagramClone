80263b94df11fa9ae2efe98a44711184fb7c9501
Dusa <revathi.mohan-dusa@capgemini.com>
2021-12-17 17:56:15 +0530

    mongodatabase connected

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 3000
+const {MONGOURI} = require('./keys')
 
-const customMiddleware = (req,res,next)=> {
-    console.log("middleware executed!")
-    next()
-}
-
-// app.use(customMiddleware)
-
-
-app.get('/', (req,res)=> {
-    console.log("home")
-    res.send("hello world")
+mongoose.connect(MONGOURI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+mongoose.connection.on('connected', ()=> {
+    console.log("connected to mongo")
 })
 
-app.get('/about', customMiddleware, (req,res)=> {
-    console.log("about")
-    res.send("about page")
+mongoose.connection.on('error', (err)=> {
+    console.log("error connecting to mongo", err)
 })
+
+
+
+
+
+
+
+
+
+
+
+// const customMiddleware = (req,res,next)=> {
+//     console.log("middleware executed!")
+//     next()
+// }
+
+// // app.use(customMiddleware)
+
+
+// app.get('/', (req,res)=> {
+//     console.log("home")
+//     res.send("hello world")
+// })
+
+// app.get('/about', customMiddleware, (req,res)=> {
+//     console.log("about")
+//     res.send("about page")
+// })
 
 app.listen(PORT, ()=> {
     console.log("server is running..", PORT);
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://mukesh:cWMUI9QhXeyyakzd@instagramclone.kaevs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
