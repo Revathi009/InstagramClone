8886e45834f38ab2f08d99aa99e3786fe966feb5
Dusa <revathi.mohan-dusa@capgemini.com>
2021-12-17 18:58:08 +0530

    password hashed

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
 
-router.get('/', (req,res)=> {
-    res.send("hello")
-})
 
 router.post('/signup', (req,res)=>{
     const {name,email,password} = req.body
@@ -16,21 +14,24 @@ router.post('/signup', (req,res)=>{
         if(savedUser){
             return res.status(422).json({error:"user already exists with that email"})
         }
-        const user = new User({
-            email, 
-            password,
-            name
-        })
-        user.save().then(user =>{
-            res.json({message:"successfully created"})
-        })
-        .catch(err=>{
-            console.log(err)
+        bcrypt.hash(password, 12).then(hashedpassword=>{
+            const user = new User({
+                email, 
+                password:hashedpassword,
+                name
+            })
+            user.save().then(user =>{
+                res.json({message:"successfully created"})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
         })
     })
     .catch(err=>{
         console.log(err)
     })
+        
 })
 
 module.exports = router;
