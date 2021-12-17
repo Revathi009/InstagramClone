d1eb194cb0a89679ee5d7230e8d96936b2360aba
Dusa <revathi.mohan-dusa@capgemini.com>
2021-12-17 17:21:20 +0530

    understanding middlewares

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const customMiddleware = (req,res,next)=> {
+    console.log("middleware executed!")
+    next()
+}
+
+// app.use(customMiddleware)
+
+
 app.get('/', (req,res)=> {
+    console.log("home")
     res.send("hello world")
+})
+
+app.get('/about', customMiddleware, (req,res)=> {
+    console.log("about")
+    res.send("about page")
 })
 
 app.listen(PORT, ()=> {
