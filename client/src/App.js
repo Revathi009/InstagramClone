a9bde91597366ece29e834a036aa95e051ce28c6
Dusa <revathi.mohan-dusa@capgemini.com>
2021-12-18 16:34:53 +0530

    react app

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react'
+import NavBar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/screens/Home';
+import Signin from './components/screens/Login';
+import Signup from './components/screens/Signup';
+import Profile from './components/screens/Profile';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar>
+
+      <Route exact path="/">
+        <Home/>
+      </Route>
+
+      <Route path="/signin">
+        <Signin/>
+      </Route>
+
+      <Route path="/profile">
+        <Profile/>
+      </Route>
+
+      <Route path="/signup">
+        <Signup/>
+      </Route>
+
+      </NavBar>
+    </BrowserRouter>
+  );
+}
+
+export default App;
