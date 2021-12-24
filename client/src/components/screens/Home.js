dfe57e0b3f0f066a4b50532e982c3e968aa0f12d
Dusa <revathi.mohan-dusa@capgemini.com>
2021-12-24 21:18:03 +0530

    Home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Home = ()=>{
     return(
-        <h1>Home</h1>
+        <div className='home'>
+            <div className='card home-card'>
+                <h5>Shruti</h5>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder='add a comment'></input>
+                </div>
+            </div>
+
+            <div className='card home-card'>
+                <h5>Shruti</h5>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder='add a comment'></input>
+                </div>
+            </div>
+
+            <div className='card home-card'>
+                <h5>Shruti</h5>
+                <div className='card-image'>
+                    <img src='https://images.unsplash.com/photo-1481833761820-0509d3217039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder='add a comment'></input>
+                </div>
+            </div>
+        </div>
     );
 }
 
